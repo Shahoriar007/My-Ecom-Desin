cc37a412e98fcdac8c4bc81b5dbb38c8cc96b7d3
Shahoriar007 <shahoriar.fahim@gmail.com>
2021-02-20 12:31:44 +0600

    Denis Ecom part-3, 34.56 min

--- Ecommerce_Django/store/models.py
+++ Ecommerce_Django/store/models.py
@@ -32,14 +32,34 @@ class Order(models.Model):
     complete = models.BooleanField(default=False)
     transaction_id = models.CharField(max_length=100, null=True)
 
-    def __str__(self):
-        return self(self.id)
+    # def __str__(self):
+    #     return self(self.id)
+
+    # @property
+    # def get_cart_total(self):
+    #     orderitems = self.orderitems_set.all()
+    #     total = sum(item.get_total for item in orderitems)
+    #     return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitems_set.all()
+        total = sum(item.quantity for item in orderitems)
+        return total
+
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Products, on_delete=models.SET_NULL, null=True, blank=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+    
+  
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
